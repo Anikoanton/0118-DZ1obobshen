ea54fb592f8206cd2a52ccb75a0a12501170e6cb
Anikoanton <anikoanton@gmail.com>
2018-02-03 04:01:24 +0300

    ok

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
         Integer[] inums  = { 1 , 2 , 3 , 4 , 5 };
 
         Mas<Integer> mas1 = new Mas<Integer>(inums);
@@ -18,6 +17,15 @@ public class Main {
         mas2.changeEllement(1,2);
 
         System.out.println("-----------");
-        
+
+
+        Box box1=new Box();
+        Apple apple1 = new Apple(2);
+        Apple apple2 = new Apple(4);
+
+        box1.addBox(apple1);
+        box1.addBox(apple2);
+        System.out.println("-----------");
+	// write your code here
     }
 }
